d8f70869e47dd27a877e0220676f30e1a69e2157
YoniA <yoniasulin@gmail.com>
2026-03-27 16:58:57 +0300

    Bufgix keep videos uninverted in dark mode

--- toggle.js
+++ toggle.js
@@ -14,7 +14,7 @@
 			const invertPct = isPdf ? '88%' : '90%';
 			style.textContent = [
 				`html { filter: invert(${invertPct}) !important; background-color: white !important; }`,
-				`img, video { filter: invert(100%) !important; }`,
+				`img, video, iframe { filter: invert(100%) !important; }`,
 			].join('\n');
 			document.head.appendChild(style);
 		}
